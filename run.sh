95667d74434ad21b511425af962f80fe22c0071b
naikok <sergiogayarre@gmail.com>
2020-01-28 07:52:04 +0100

    fix in command

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # Un script simple
 docker-compose up -d --build
+sudo docker stop container_redis
+sudo docker start container_redis
+sudo docker stop symfonydockerized_php_1
+sudo docker start symfonydockerized_php_1
+sudo docker stop container_mysql
+sudo docker start container_mysql
+sudo docker stop container_nginx
+sudo docker start container_nginx
+
